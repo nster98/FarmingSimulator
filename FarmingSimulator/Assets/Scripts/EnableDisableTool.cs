6ffd3b8a8bd77dd6b9df5bad117c3d475db51751
117alaemmle <atycowsky@gmail.com>
2020-05-03 12:52:59 -0400

    Added tool switching
    I don't know what the axe is for

---
+++ FarmingSimulator/Assets/Scripts/EnableDisableTool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisableTool : MonoBehaviour
+{
+    public GameObject tool1;
+    public GameObject tool2;
+    public GameObject tool3;
+
+    private GameObject currtool;
+    //private int numberobjects = 3;
+    //private float delay = 2.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //currtool.gameObject.SetActive(false);
+            //currtool = tool1;
+            tool3.gameObject.SetActive(false);
+            tool2.gameObject.SetActive(false);
+            tool1.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            tool1.gameObject.SetActive(false);
+            tool3.gameObject.SetActive(false);
+            tool2.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            tool1.gameObject.SetActive(false);
+            tool2.gameObject.SetActive(false);
+            tool3.gameObject.SetActive(true);
+        }
+    }
+}
