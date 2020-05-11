5dc38f8e8a791e24dbc149725bfdb1e8727718a3
Nathan Glikman <nster98@gmail.com>
2020-05-11 14:50:00 -0400

    Added corn growing plus materials

---
+++ FarmingSimulator/Assets/PlantGrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantGrow : MonoBehaviour
+{
+    public int amountOfParts;
+    public GameObject[] partsOfPlant;
+    public int secondsToGrow;
+
+    private float timer = 0;
+
+    private int index = 0;
+
+
+    void Start()
+    {
+        foreach (GameObject part in partsOfPlant)
+        {
+            part.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime; // Timer
+
+        if (timer > secondsToGrow)
+        {
+            partsOfPlant[index].SetActive(true);
+
+            if (index <= amountOfParts)
+                index++;
+            timer = 0;
+        }
+    }
+}
---
+++ FarmingSimulator/Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject tool1; // Shovel
+    public GameObject tool2; // Hoe
+    public GameObject tool3; // Seedbag
+
+    public Camera cam;
+
+    public GameObject corn;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clicked?");
+            var screenPos = Input.mousePosition;
+            var ray = cam.ScreenPointToRay(screenPos);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Debug.Log(hit.collider.name);
+                if (hit.collider.name == "Terrain")
+                {
+                    Instantiate(corn, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f));
+                    Debug.Log(ray);
+                }
+                else
+                    Debug.Log("Plane clicked but raycast hit something else");
+            }
+            else
+            {
+                Debug.Log("Plane was clicked but raycast missed");
+            }
+        }
+    }
+}
