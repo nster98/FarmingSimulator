f86592ba7ef4dcb5f7edad9c665185ab80e0e50c
117alaemmle <atycowsky@gmail.com>
2020-05-13 10:52:55 -0400

    Added time and day-night

---
+++ FarmingSimulator/Assets/Scripts/SunBehavior.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SunBehavior : MonoBehaviour
+{
+    public Material Skybox1;
+    public Material Skybox2;
+
+   
+    public Light sun;
+    public float secondsInFullDay = 120f;
+    [Range(0, 1)]
+    public float currentTimeOfDay = 0;
+    [HideInInspector]
+    public float timeMultiplier = 1f;
+
+    float sunInitialIntensity;
+
+    void Start()
+    {
+        sunInitialIntensity = sun.intensity;
+        RenderSettings.skybox = Skybox1;
+    }
+    private void OnMouseDown()
+    {
+        currentTimeOfDay = 0.3F;
+        
+    }
+    void Update()
+    {
+        UpdateSun();
+
+        currentTimeOfDay += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
+
+        if (currentTimeOfDay >= 1)
+        {
+            currentTimeOfDay = 0;
+        }
+        if(currentTimeOfDay>0.8 || currentTimeOfDay < 0.2)
+        {
+            RenderSettings.skybox = Skybox2;
+        }
+        else
+        {
+            RenderSettings.skybox = Skybox1;
+        }
+        
+    }
+
+    void UpdateSun()
+    {
+        sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
+
+        float intensityMultiplier = 1;
+        if (currentTimeOfDay <= 0.23f || currentTimeOfDay >= 0.75f)
+        {
+            intensityMultiplier = 0;
+        }
+        else if (currentTimeOfDay <= 0.25f)
+        {
+            intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 0.23f) * (1 / 0.02f));
+        }
+        else if (currentTimeOfDay >= 0.73f)
+        {
+            intensityMultiplier = Mathf.Clamp01(1 - ((currentTimeOfDay - 0.73f) * (1 / 0.02f)));
+        }
+
+        sun.intensity = sunInitialIntensity * intensityMultiplier;
+    }
+}
